a150f3894608a5caa17b0b15b96a8aaf1bcea227
Angelbenites <Angelbenites@gmail.com>
2024-01-30 11:57:43 -0500

    Agregacion de carpeta Python

---
+++ python/python1.py
@@ -0,0 +1,24 @@
+#Imprimer cualquier texto
+#Crea variables, suma, resta, divide y multiplica
+#Crea un producto y sacale igv
+
+#1. ejercicio 
+
+#print("Hola mundo")
+
+#2. ejercicio 
+
+number1=int(input("Ingresa primer numero: "))
+number2=int(input("Ingresa segundo numero: "))
+
+sumar=number1+number2
+restar=number1-number2
+dividir=number1/number2
+multiplicar=number1*number2
+
+
+
+print(" La suma es: ", sumar
+    ,"\n La resta es: ", restar 
+    , "\n El producto es: ", multiplicar
+    , "\n El residuo es: ", dividir)
